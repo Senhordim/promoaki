1ce386da50f9c0f3b76a648e61c5dc00ca9985f4
Diego Collares <diego.collares@gmail.com>
2017-02-17 17:07:49 -0200

    criado modelo Address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217190048) do
+ActiveRecord::Schema.define(version: 20170217190504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 20170217190048) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.index ["store_id"], name: "index_addresses_on_store_id", using: :btree
+  end
+
+  create_table "segments", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "stores", force: :cascade do |t|
