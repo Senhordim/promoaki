4a25e887314d863c71db09b4bf830cfa64963a86
Diego Collares <diego.collares@gmail.com>
2017-02-17 18:26:53 -0200

    criado o namespace ad e controller para home index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
   devise_for :stores
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :ad do
+    get 'dashboard/index'
+    root 'dashboard#index'
+  end
+
+  get 'home/index'
+  root to: 'home#index'
+
 end
