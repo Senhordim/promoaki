76ce2215a8a48cd4df5dab3905a874864cac1f19
Diego Collares <diego.collares@gmail.com>
2017-03-07 16:52:59 -0300

    load schema fle

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217191146) do
+ActiveRecord::Schema.define(version: 20170307192726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,10 @@ ActiveRecord::Schema.define(version: 20170217191146) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "social_name"
+    t.string   "fantasy_name"
+    t.string   "cnpj"
+    t.string   "phone"
     t.index ["email"], name: "index_stores_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_stores_on_reset_password_token", unique: true, using: :btree
   end
