2a238c84aae2ebee8318a2ba9e18136fb274f0f7
Diego Collares <diego.collares@gmail.com>
2017-03-08 20:31:19 -0300

    test if cnpj is unique

--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -31,6 +31,7 @@ RSpec.describe Store, type: :model do
    it { should validate_presence_of(:social_name ) }
    it { should validate_presence_of(:fantasy_name ) }
    it { should validate_presence_of(:cnpj ) }
+   it { should validate_uniqueness_of(:cnpj ) }
    it { should validate_presence_of(:phone ) }
 
    it { is_expected.to have_one(:address)}
