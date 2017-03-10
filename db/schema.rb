11a4b385a6bfa48315d9958c0506bf0c71656fd1
Diego Collares <diego.collares@gmail.com>
2017-03-10 17:25:00 -0300

    load schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307192726) do
+ActiveRecord::Schema.define(version: 20170310141811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 20170307192726) do
     t.integer  "store_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.float    "latitude"
+    t.float    "longitude"
+    t.string   "country"
     t.index ["store_id"], name: "index_addresses_on_store_id", using: :btree
   end
 
