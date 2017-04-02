f939e18d81884acdf0354039cb251bb0857a5e74
jean-felipe <mandakeru@gmail.com>
2017-04-02 00:57:44 -0300

    endpoint finished

--- app/controllers/api/v1/promotions_controller.rb
+++ app/controllers/api/v1/promotions_controller.rb
@@ -1,15 +1,42 @@
 class Api::V1::PromotionsController < Api::ApiController
-  respond_to :json
- 
+   
   def index
-  	require 'pry'; binding.pry
   	@longitude = params[:longitude]
   	@latitude  = params[:latitude]
 
-   # Address.near([-25.594798, -49.339072], 5)
-    @address = Address.near([@longitude, @latitude], 15)
+  	if some_address? || find_promotion?
+  	  require 'pry'; binding.pry
+  	  render :json => { :status => "error", :message => "No pomotions yet" }
+  	else
+	  # Address.near([-25.594798, -49.339072], 5)
+	  @address = Address.near([@longitude, @latitude], 15)
+	  find_promotions(@address)
+	  render_sucess(@promotions)
+    end
+  end
 
-      require 'pry'; binding.pry
+
+  private
+  
+  def find_promotions(addresses)
+  	@addresses = addresses
+  	@promotions = []
+  	@addresses.each do |promo|
+  	  @promotions << Promotion.find_by(store_id: promo.store_id)
+  	end
+  	@promotions
+  end
+
+  def find_promotion?
+  	Promotion.nil?
+  end
+
+  def render_sucess(object)
+  	render :json => object, :status => 200
+  end
+
+  def some_address?
+  	Address.geocoded.nil?
   end
 end
 
--- spec/controllers/api/v1/promotions_api_controller_spec.rb
+++ spec/controllers/api/v1/promotions_api_controller_spec.rb
@@ -11,22 +11,19 @@ describe Api::V1::PromotionsController do
 
 	  store1 = create(:store)
 	  store2 = create(:store)
-	  store3 = create(:store)
-	  store4 = create(:store)
-	  store5 = create(:store)
-	  
-
+	    
 	  address1 = create(:address, store_id: store1.id, street: 'Pia Lazari Bertoldi', numb: '3',  city: 'Curitiba', state: 'PR', country: 'Brasil')
 	  address2 = create(:address, store_id: store2.id, street: 'Augusto Bertoldi',    numb: '13', city: 'Curitiba', state: 'PR', country: 'Brasil')
-	  address3 = create(:address, store_id: store3.id, street: 'Marcos Bertoldi',     numb: '23', city: 'Curitiba', state: 'PR', country: 'Brasil')
-	  address4 = create(:address, store_id: store4.id, street: 'Adolfo Ançay',        numb: '33', city: 'Curitiba', state: 'PR', country: 'Brasil')
-	  address5 = create(:address, store_id: store5.id, street: 'Neusa Gusso',         numb: '43', city: 'Curitiba', state: 'PR', country: 'Brasil')
+	  
+	  promotion1 = create(:promotion, store_id: store1.id)
+	  promotion2 = create(:promotion, store_id: store2.id)
   	end
 
-  	it "should find all promotions" do
-  	Address.near([-25.594798, -49.339072], 5)
-
+  	it "should find promotions near" do
   	  post :index, params: { longitude: @longitude, latitude: @latitude}
+  	  json = JSON.parse(response.body)
+  	  expect(response).to be_success
+  	  expect(json.length).to eq(2)
   	end
   	
   end
