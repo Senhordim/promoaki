03510552380f3707bff6402be580f17178aa1cb8
Diego Collares <diego.collares@gmail.com>
2017-04-12 13:08:36 -0300

    clear routes to project

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-
+  # Autenticação - Loja
   devise_for :stores, controllers: {
     sessions:      'stores/sessions',
     registrations: 'stores/registrations',
@@ -8,24 +8,22 @@ Rails.application.routes.draw do
     passwords:     'stores/passwords'
   }
 
+  # Administrativo
   namespace :ad do
-
     resources :addresses
     resources :promotions
-
     get 'dashboard/index'
     root 'dashboard#index'
-  
   end
 
+  # API
   namespace :api, path: '', defaults: { format: 'json'} do
     namespace :v1 do
        post "/promotions", :to => "promotions#index", as: 'promotions'
-      
     end
-    
   end
 
+  # Rotas
   get 'home/index'
   root to: 'home#index'
 
