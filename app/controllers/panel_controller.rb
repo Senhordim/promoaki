1b2ee9f9b10ea5063273089c290a8af36d1e1074
Diego Collares <diego.collares@gmail.com>
2017-05-04 23:39:43 -0300

    created panel controller

--- app/controllers/panel_controller.rb
+++ app/controllers/panel_controller.rb
@@ -1,8 +1,14 @@
 class PanelController < ApplicationController
 
-  layout 'ad'
+  layout 'panel'
 
   # before_action :authenticate_store!, :except => [:api_promotions]
   before_action :authenticate_admin!
+
+  private
+
+  def after_sign_in_path_for(resource)
+    panel_root_path
+  end
 
 end
