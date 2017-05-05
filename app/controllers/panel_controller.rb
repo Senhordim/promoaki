66007d3dfd3d276306957a527c65b7ca290f9671
Diego Collares <diego.collares@gmail.com>
2017-05-05 00:36:30 -0300

    config pudint panel

--- app/controllers/panel_controller.rb
+++ app/controllers/panel_controller.rb
@@ -5,7 +5,16 @@ class PanelController < ApplicationController
   # before_action :authenticate_store!, :except => [:api_promotions]
   before_action :authenticate_admin!
 
+  def pundit_user
+    current_admin
+  end
+
   private
+
+  def user_not_authorized
+    flash[:alert] = "Você não tem autorização para executar essa ação!"
+    redirect_to(request.referrer || backoffice_root_path)
+  end
 
   def after_sign_in_path_for(resource)
     panel_root_path
