f0890c57c98cf732aae30361a5bf7998d414e6a8
Diego Collares <diego.collares@gmail.com>
2017-05-04 23:37:52 -0300

    hange ad controller

--- app/controllers/ad_controller.rb
+++ app/controllers/ad_controller.rb
@@ -5,4 +5,26 @@ class AdController < ApplicationController
   # before_action :authenticate_store!, :except => [:api_promotions]
   before_action :authenticate_store!
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(
+        :email, :password,
+        :password_confirmation,
+        :phone,
+        :cnpj,
+        :social_name,
+        :fantasy_name)
+    end
+  end
+
+  private
+
+  def after_sign_in_path_for(resource)
+    ad_root_path
+  end
+
 end
