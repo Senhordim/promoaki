eb20dbae4b2c1950cfef90cf63fa6175085c9016
Diego Collares <diego.collares@gmail.com>
2017-05-05 00:37:03 -0300

    add enum and scope

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,10 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  enum role: [ :full_access, :restrict_acess ]
+
+  scope :with_full_access, -> { where(role: 0) }
+  scope :with_restricted_access, -> { where(role: 1) }
+
 end
