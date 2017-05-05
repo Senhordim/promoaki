24e61e50961cbdbf493c839b866922eeadcfa1c3
Diego Collares <diego.collares@gmail.com>
2017-05-04 23:38:41 -0300

    remove methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit(
-        :email, :password,
-        :password_confirmation,
-        :phone,
-        :cnpj,
-        :social_name,
-        :fantasy_name)
-    end
-  end
-
-  private
-
-  def after_sign_in_path_for(resource)
-    ad_root_path
-  end
-
 end
