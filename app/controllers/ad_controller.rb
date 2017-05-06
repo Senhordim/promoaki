1679e3ce7da8798881518297edbd815b4073b106
Diego Collares <diego.collares@gmail.com>
2017-05-06 16:45:56 -0300

    change ntroller signup

--- app/controllers/ad_controller.rb
+++ app/controllers/ad_controller.rb
@@ -5,22 +5,6 @@ class AdController < ApplicationController
   # before_action :authenticate_store!, :except => [:api_promotions]
   before_action :authenticate_store!
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit(
-        :email, :password,
-        :password_confirmation,
-        :phone,
-        :cnpj,
-        :social_name,
-        :fantasy_name)
-    end
-  end
-
   private
 
   def after_sign_in_path_for(resource)
