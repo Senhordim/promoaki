841a21d66f923e0771d7f9d56faf46012e8e6cb6
Diego Collares <diego.collares@gmail.com>
2017-05-07 09:21:48 -0300

    move configure_permitted_parameters to registration controoler

--- app/controllers/stores/registrations_controller.rb
+++ app/controllers/stores/registrations_controller.rb
@@ -2,17 +2,30 @@ class Stores::RegistrationsController < Devise::RegistrationsController
 
   layout :admin_layout
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   private
 
-    def admin_layout
-      current_store.present? ? 'ad' : 'auth'
-    end
-
+  def admin_layout
+    current_store.present? ? 'ad' : 'auth'
+  end
 
   protected
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(
+        :email, :password,
+        :password_confirmation,
+        :phone,
+        :cnpj,
+        :social_name,
+        :fantasy_name)
+    end
+  end
+
   def after_update_path_for(resource)
-    ad_root_path
+    store_session_path
   end
 
 end
