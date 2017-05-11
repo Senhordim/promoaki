961e99f8d1096358476c2d7fff6e34a629d23170
Diego Collares <diego.collares@gmail.com>
2017-05-11 19:05:40 -0300

    config setup pudint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,13 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  private
 
+  def user_not_authorized
+    flash[:alert] = "Você não tem autorização para executar essa ação!"
+    redirect_to(request.referrer || panel_root_path)
+  end
 
 end
